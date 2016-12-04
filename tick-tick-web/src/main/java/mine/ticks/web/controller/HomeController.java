083e929f32c397ede524fc4441c6e88d79a4af61
ddn4free <hughrover@163.com>
2016-12-04 16:35:21 +0800

    push

--- tick-tick-web/src/main/java/mine/ticks/web/controller/HomeController.java
+++ tick-tick-web/src/main/java/mine/ticks/web/controller/HomeController.java
@@ -1,18 +1,15 @@
 package mine.ticks.web.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 /**
  * User: chengcheng.liu
  * Date: 16/11/22
  */
 
-@Controller
-@RequestMapping("/index.html")
-public class HomeController {
-    @RequestMapping
-    public String home() {
-        return "hello";
-    }
-}
+//@Controller
+//@RequestMapping("/index")
+//public class HomeController {
+//    @RequestMapping
+//    public String home() {
+//        return "hello";
+//    }
+//}
